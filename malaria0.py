06c1dff0c28da4964bb154e7a8c5b5245b19f76a
josepbf <72404735+josepbf@users.noreply.github.com>
2022-01-20 01:02:10 +0100

    Update malaria0.py

--- malaria0.py
+++ malaria0.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from numpy import load
 from numpy import expand_dims
-from matplotlib import pyplot
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 import os
